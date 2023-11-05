b04c7920e84b3afdcdfd38f32e80cd8c6ce43e9a
n3wsk1y <miner.fedenko@gmail.com>
2023-11-05 22:53:43 +0300

    Support feature

--- src/support/dto/message.dto.ts
+++ src/support/dto/message.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class MessageDto {
     @IsString()
+    @IsNotEmpty()
     text: string;
 }
