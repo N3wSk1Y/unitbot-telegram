560b6c6a2a8ad58c32e6fc7006259a4639af664c
n3wsk1y <miner.fedenko@gmail.com>
2023-11-12 16:05:55 +0300

    Media in messages feature

--- src/local-file/local-file.service.ts
+++ src/local-file/local-file.service.ts
@@ -40,7 +40,7 @@ export class LocalFileService {
         const mimetype = res.headers["content-type"] as string;
 
         const fileName = (Math.random() * 10000).toString();
-        const localFilePath = path.join(process.cwd(), localPath, fileName);
+        const localFilePath = path.join(process.cwd(), "/files", localPath, fileName);
         const dir = path.join(localPath);
         if (!fsSync.existsSync(dir)) {
             await fs.mkdir(dir);
